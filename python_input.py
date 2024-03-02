2e291aa905069dc3e4dbd2569bf6bb166698a0f0
cpphey <cpphey@gmail.com>
2024-03-02 00:48:01 -0500

    test

---
+++ python_input.py
@@ -0,0 +1,14 @@
+def a():
+    var = input("enter something:")
+    print (var)
+
+    #var = raw_input("enter something:") #older version
+    print (var)
+def b():
+       print (one)
+    print (two)
+
+def c(arg):
+    return (square := arg**2) / (square**2 / arg ) #assignment_expression walrus operator
+
+print ( c(10.01) )
