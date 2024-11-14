8bae08ccc33af6013410c481a871333e5a77fcf5
cpphey <cpphey@gmail.com>
2024-11-14 17:07:38 -0500

    Bond2 still works with Thirty360.USA

--- bondTest/main.py
+++ bondTest/main.py
@@ -1,40 +1,52 @@
 import QuantLib as ql
+import datetime
+
+
 
 #Input data
-# issue_date = ql.Date(1, 11, 2024)
-# end_date = ql.Date(3, 6, 2030)
-# coupon_rate = 0.04513
-# price = 100.171
+# issue_date = ql.Date(26, 11, 2024)
+# end_date = ql.Date(1, 12, 2032)
+# coupon_rate = 0.06875
+# price = 100.86951
 # settlement_days = 2
 # face_value = 100.0
 
 issue_date = ql.Date(1, 11, 2024)
-end_date = ql.Date(1, 11, 2031)
-coupon_rate = 0.06625
-price = 101.0625
+end_date = ql.Date(3, 6, 2030)
+coupon_rate = 0.04513
+price = 100.171
 settlement_days = 2
 face_value = 100.0
 
+# issue_date = ql.Date(1, 11, 2024)
+# end_date = ql.Date(1, 11, 2031)
+# coupon_rate = 0.06625
+# price = 101.0625
+# settlement_days = 2
+# face_value = 100.0
+
 
 # Setup schedule and bond
-valuation_date = '2024-11-01'
-todays_date = ql.DateParser.parseFormatted('2024-11-01', '%Y-%m-%d')
-print("Todays Date is: "+valuation_date)
+today = datetime.date.today()
+formatted_date = today.strftime('%Y-%m-%d')
+todays_date = ql.DateParser.parseFormatted(formatted_date, '%Y-%m-%d')
+print("Todays Date is: "+formatted_date)
 ql.Settings.instance().evaluationDate = todays_date
 
 schedule = ql.Schedule(issue_date, end_date, ql.Period(ql.Semiannual),
                        ql.NullCalendar(), ql.Unadjusted, ql.Unadjusted,
                        ql.DateGeneration.Backward, False)
 
-bond = ql.FixedRateBond(settlement_days, face_value, schedule, [coupon_rate], ql.ActualActual(ql.ActualActual.Actual365))
+day_counter = ql.Thirty360(ql.Thirty360.USA)
+bond = ql.FixedRateBond(settlement_days, face_value, schedule, [coupon_rate], day_counter)
 
 # Yield to Maturity (YTM)
 clean_price = price
-ytm = bond.bondYield(clean_price, ql.ActualActual(ql.ActualActual.Actual365), ql.Compounded, ql.Semiannual)
+ytm = bond.bondYield(clean_price, day_counter, ql.Compounded, ql.Semiannual)
 print(f"Yield to Maturity (YTM): {ytm * 100:.2f}%")
 
 # Duration and Convexity
-day_counter = ql.ActualActual(ql.ActualActual.Actual365)
+
 yield_rate = ql.InterestRate(ytm, day_counter, ql.Compounded, ql.Semiannual)
 
 # Calculate duration and convexity
