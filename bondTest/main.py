b2b46480572c8733b191b21bb7b01e6489ed2757
cpphey <cpphey@gmail.com>
2024-11-14 16:43:57 -0500

    Actual365 was put

--- bondTest/main.py
+++ bondTest/main.py
@@ -1,19 +1,19 @@
 import QuantLib as ql
 
 #Input data
-issue_date = ql.Date(1, 11, 2024)
-end_date = ql.Date(3, 6, 2030)
-coupon_rate = 0.04513
-price = 100.171
-settlement_days = 2
-face_value = 100.0
-
 # issue_date = ql.Date(1, 11, 2024)
-# end_date = ql.Date(1, 11, 2031)
-# coupon_rate = 0.06625
-# price = 101.0625
+# end_date = ql.Date(3, 6, 2030)
+# coupon_rate = 0.04513
+# price = 100.171
 # settlement_days = 2
 # face_value = 100.0
+
+issue_date = ql.Date(1, 11, 2024)
+end_date = ql.Date(1, 11, 2031)
+coupon_rate = 0.06625
+price = 101.0625
+settlement_days = 2
+face_value = 100.0
 
 
 # Setup schedule and bond
@@ -26,15 +26,15 @@ schedule = ql.Schedule(issue_date, end_date, ql.Period(ql.Semiannual),
                        ql.NullCalendar(), ql.Unadjusted, ql.Unadjusted,
                        ql.DateGeneration.Backward, False)
 
-bond = ql.FixedRateBond(settlement_days, face_value, schedule, [coupon_rate], ql.ActualActual(ql.ActualActual.ISDA))
+bond = ql.FixedRateBond(settlement_days, face_value, schedule, [coupon_rate], ql.ActualActual(ql.ActualActual.Actual365))
 
 # Yield to Maturity (YTM)
 clean_price = price
-ytm = bond.bondYield(clean_price, ql.ActualActual(ql.ActualActual.ISDA), ql.Compounded, ql.Semiannual)
+ytm = bond.bondYield(clean_price, ql.ActualActual(ql.ActualActual.Actual365), ql.Compounded, ql.Semiannual)
 print(f"Yield to Maturity (YTM): {ytm * 100:.2f}%")
 
 # Duration and Convexity
-day_counter = ql.ActualActual(ql.ActualActual.ISDA)
+day_counter = ql.ActualActual(ql.ActualActual.Actual365)
 yield_rate = ql.InterestRate(ytm, day_counter, ql.Compounded, ql.Semiannual)
 
 # Calculate duration and convexity
@@ -43,5 +43,5 @@ duration_modified = ql.BondFunctions.duration(bond, yield_rate, ql.Duration.Modi
 convexity = ql.BondFunctions.convexity(bond, yield_rate) / 100
 
 print(f"Simple Duration: {duration_simple:.8f}")
-print(f"Modified Duration: {duration_modified:.8f}")
+#print(f"Modified Duration: {duration_modified:.8f}")
 print(f"Convexity: {convexity:.8f}")
