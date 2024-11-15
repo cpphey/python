e9e75cf03de74f15db8a32329867736a2ce83d71
cpphey <cpphey@gmail.com>
2024-11-15 08:58:33 -0500

    Treasury Works

--- bondTest/main.py
+++ bondTest/main.py
@@ -4,27 +4,42 @@ import datetime
 
 
 #Input data
+#559665AB0
+#US559665AB08
 # issue_date = ql.Date(26, 11, 2024)
 # end_date = ql.Date(1, 12, 2032)
 # coupon_rate = 0.06875
 # price = 100.86951
 # settlement_days = 2
 # face_value = 100.0
+#bondCalendar = ql.UnitedStates()
 
-issue_date = ql.Date(1, 11, 2024)
-end_date = ql.Date(3, 6, 2030)
-coupon_rate = 0.04513
-price = 100.171
-settlement_days = 2
-face_value = 100.0
+# issue_date = ql.Date(1, 11, 2024)
+# end_date = ql.Date(3, 6, 2030)
+# coupon_rate = 0.04513
+# price = 100.171
+# settlement_days = 2
+# face_value = 100.0
+#bondCalendar = ql.UnitedStates()
 
+#https://treasurydirect.gov/auctions/announcements-data-results/
+#https://treasurydirect.gov/auctions/auction-query/?cusip=912810UE6
 # issue_date = ql.Date(1, 11, 2024)
 # end_date = ql.Date(1, 11, 2031)
 # coupon_rate = 0.06625
 # price = 101.0625
 # settlement_days = 2
 # face_value = 100.0
+bondCalendar = ql.Canada()
 
+#https://treasurydirect.gov/auctions/auction-query/?cusip=912810UE6
+issue_date = ql.Date(15, 11, 2024)
+end_date = ql.Date(15, 11, 2054)
+coupon_rate = 0.045
+price = 98.253773
+settlement_days = 2
+face_value = 100.0
+#bondCalendar = ql.UnitedStates()
 
 # Setup schedule and bond
 today = datetime.date.today()
@@ -33,8 +48,16 @@ todays_date = ql.DateParser.parseFormatted(formatted_date, '%Y-%m-%d')
 print("Todays Date is: "+formatted_date)
 ql.Settings.instance().evaluationDate = todays_date
 
+#https://github.com/lballabio/QuantLib/blob/1aae34679f0abd852b6dc90c72cd6deafbdb5c1e/ql/time/schedule.hpp
+#https://github.com/lballabio/QuantLib/blob/1aae34679f0abd852b6dc90c72cd6deafbdb5c1e/ql/time/schedule.cpp
+# schedule = ql.Schedule(issue_date, end_date, ql.Period(ql.Semiannual),
+#                        bondCalendar , ql.Unadjusted, ql.Unadjusted,
+#                        ql.DateGeneration.Backward, False)
+#https://github.com/lballabio/QuantLib/blob/1aae34679f0abd852b6dc90c72cd6deafbdb5c1e/ql/time/businessdayconvention.hpp#L41
+#https://github.com/lballabio/QuantLib/blob/1aae34679f0abd852b6dc90c72cd6deafbdb5c1e/ql/time/dategenerationrule.hpp#L39
+#https://github.com/lballabio/QuantLib/blob/1aae34679f0abd852b6dc90c72cd6deafbdb5c1e/ql/compounding.hpp#L32
 schedule = ql.Schedule(issue_date, end_date, ql.Period(ql.Semiannual),
-                       ql.NullCalendar(), ql.Unadjusted, ql.Unadjusted,
+                       bondCalendar , ql.Following , ql.Following ,
                        ql.DateGeneration.Backward, False)
 
 day_counter = ql.Thirty360(ql.Thirty360.USA)
@@ -42,12 +65,14 @@ bond = ql.FixedRateBond(settlement_days, face_value, schedule, [coupon_rate], da
 
 # Yield to Maturity (YTM)
 clean_price = price
-ytm = bond.bondYield(clean_price, day_counter, ql.Compounded, ql.Semiannual)
-print(f"Yield to Maturity (YTM): {ytm * 100:.2f}%")
+ytm = bond.bondYield(clean_price, day_counter, ql.Compounded, ql.Semiannual) #SimpleThenCompounded
+print(f"Yield to Maturity (YTM): {ytm * 100:.4f}%")
+# bond.dirtyPrice()
+# print(str())
 
 # Duration and Convexity
-
-yield_rate = ql.InterestRate(ytm, day_counter, ql.Compounded, ql.Semiannual)
+#yield_rate = ql.InterestRate(ytm, day_counter, ql.Compounded, ql.Semiannual)#SimpleThenCompounded
+yield_rate = ql.InterestRate(ytm, day_counter, ql.SimpleThenCompounded, ql.Semiannual)
 
 # Calculate duration and convexity
 duration_simple = ql.BondFunctions.duration(bond, yield_rate, ql.Duration.Simple)
