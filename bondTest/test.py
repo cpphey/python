65f8c21ab6b50b26ac4ad7a84a6ae357c8255b54
cpphey <cpphey@gmail.com>
2024-11-19 16:51:29 -0500

    p4 ytm matched again

--- bondTest/test.py
+++ bondTest/test.py
@@ -1,19 +1,19 @@
 import QuantLib as ql
 import datetime
 
-#2AS5
-issue_date = ql.Date(10,5,2017)
-end_date = ql.Date(15,3,2027)
+#p4
+issue_date = ql.Date(28, 10, 2014)
+end_date = ql.Date(1, 5, 2025)
 coupon_rate = 0.05
-price = 100.458
-settlement_days = 1
+price = 99.704
+settlement_days = 0
 face_value = 100.0
-bondCalendar = ql.UnitedStates(ql.UnitedStates.Settlement)
+bondCalendar =  ql.UnitedStates(ql.UnitedStates.Settlement)
 day_counter = ql.Thirty360(ql.Thirty360.USA)
-#day_counter  = ql.ActualActual(ql.ActualActual.ISMA)
+#day_counter  = ql.ActualActual(ql.ActualActual.ISMA)@al
 
 # Setup schedule and bond
-custom_date = None #'2024-11-15'
+custom_date = None #'2024-11-18'
 today = datetime.date.today()
 formatted_date = today.strftime('%Y-%m-%d') if custom_date is None else custom_date
 todays_date = ql.DateParser.parseFormatted(formatted_date, '%Y-%m-%d')
@@ -35,12 +35,11 @@ bond = ql.FixedRateBond(settlement_days, face_value, schedule, [coupon_rate], da
 # Yield to Maturity (YTM)
 clean_price = price
 ytm = bond.bondYield(clean_price, day_counter, ql.Compounded, ql.Semiannual) #SimpleThenCompounded
-ytm = bond.bondYield(clean_price, day_counter, ql.Compounded, ql.Semiannual)
 print(f"Yield to Maturity (YTM): {ytm * 100:.8f}%")
 
 # Duration and Convexity
 #yield_rate = ql.InterestRate(ytm, day_counter, ql.Compounded, ql.Semiannual)#SimpleThenCompounded
-yield_rate = ql.InterestRate(ytm, day_counter, ql.SimpleThenCompounded, ql.Semiannual)
+yield_rate = ql.InterestRate(ytm, day_counter, ql.CompoundedThenSimple, ql.Semiannual)
 
 # Calculate duration and convexity
 duration_simple = ql.BondFunctions.duration(bond, yield_rate, ql.Duration.Simple)
@@ -48,5 +47,5 @@ duration_modified = ql.BondFunctions.duration(bond, yield_rate, ql.Duration.Modi
 convexity = ql.BondFunctions.convexity(bond, yield_rate) / 100
 
 print(f"Simple Duration: {duration_simple:.8f}")
-#print(f"Modified Duration: {duration_modified:.8f}")
+# print(f"Modified Duration: {duration_modified:.8f}")
 print(f"Convexity: {convexity:.8f}")
